3f90250a704474b6779ac090b6bc8af467a438e6
felipetaua <taua.felipee@gmail.com>
2025-02-17 14:50:38 -0300

    iniciando cadastro de pessoas na tela1

--- lib/Tela1.dart
+++ lib/Tela1.dart
@@ -25,12 +25,50 @@ class Cadastro extends StatefulWidget {
 
 // classe que tem quantas alterações forem necessárias
 class _CadastroState extends State<Cadastro> {
+  // Controle dos inputs do formulário
+  final nomeControle = TextEditingController();
+  final emailControle = TextEditingController();
+  final telefoneControle = TextEditingController();
+  final enderecoControle = TextEditingController();
+  final cidadeControle = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Cadastro de Pessoas'),
         backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              "Cadastro de Contato",
+              style: TextStyle(fontSize: 24),
+            ),
+            TextField(
+              controller: nomeControle,
+              decoration: InputDecoration(labelText: 'Nome'),
+            ),
+            TextField(
+              controller: emailControle,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: telefoneControle,
+              decoration: InputDecoration(labelText: 'Telefone'),
+            ),
+            TextField(
+              controller: enderecoControle,
+              decoration: InputDecoration(labelText: 'Endereço'),
+            ),
+            TextField(
+              controller: cidadeControle,
+              decoration: InputDecoration(labelText: 'Cidade'),
+            ),
+          ],
+        ),
       ),
     );
   }
